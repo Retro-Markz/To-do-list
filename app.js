311041a09d3effd02680681549d821b254362b9f
Retro-Markz <115151977+Retro-Markz@users.noreply.github.com>
2023-06-24 03:30:41 -0700

    to do list con fecha y lista... to do list Date

--- app.js
+++ app.js
@@ -8,41 +8,22 @@ app.set('view engine', 'ejs');
 app.get("/", function (req, res) {
 
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    
+   
 
-    switch (currentDay) {
-        case 0:
-            day = "domingo";
-            break;
-        case 1:
-            day = "lunes";
-            break;
-        case 2:
-            day = "martes";
-            break;
-        case 3:
-            day = "miercoles";
-            break;
-        case 4:
-            day = "jueves";
-            break;
-        case 5:
-            day = "viernes";
-            break;
-        case 6:
-            day = "sabado";
-            break;
+    const options = {
+        weekday: "long",    
+        day: "numeric",
+        month: "long"
+    };
 
-        default:
-            console.log("error: el dia es: " + currentDay);
-           
-    }
-|
+    var day = today.toLocaleDateString("en-US", options);       
+    
+
     res.render("list", { KindOfDay: day });
 
 });
 
 app.listen(3000, function () {
-    console.log("server is }runing on port 3000");
+    console.log("server is runing on port 3000");
 });
