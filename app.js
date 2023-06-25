447a5c088be305af791739226f9208bc48e53149
Retro-Markz <115151977+Retro-Markz@users.noreply.github.com>
2023-06-25 06:18:54 -0700

    almost done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = [];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -26,7 +27,7 @@ app.get("/", function (req, res) {
     let day = today.toLocaleDateString("en-US", options);       
     
 
-    res.render("list", { KindOfDay: day, newListItems: items});
+    res.render("list", { ListTitle: day, newListItems: items});
 
 });
 
@@ -39,6 +40,26 @@ app.get("/", function (req, res) {
       res.redirect("/");
      
     });
+
+
+
+    app.get("/work", function (req, res) {
+
+        res.render("list", {ListTitle: "Work List", newListItems: workItems});
+    });
+
+    app.post("/work", function(req, res){
+
+        let item = req.body.newItem;
+        workItems.push(item);
+        res.redirect("/work");  
+    })
+
+
+
+
+
+
 
 app.listen(3000, function () {
     console.log("server is runing on port 3000");
